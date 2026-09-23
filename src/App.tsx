83c32abbd2a118cb34e9d46fb826184835f91258
Fernando R. Ramos <fer469@gmail.com>
2026-09-23 10:13:01 -0300

    fix: use HashRouter and 404.html redirect to fix GitHub Pages reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { RevisionPage } from './pages/RevisionPage';
 
